ea10873a9ed5488d2ee88e15212fa4f0f1a0ab99
Keef <keith.short32@gmail.com>
2016-04-04 16:47:51 +0100

    - SQL Table updater for location now only adds if there are not already entries - mysql.js now has a function for creating jsons, added sql queries for both daily and hourly auth counts - improved hourly auth chart, now fills in all missing hours and makes the count = 0

--- genjson.js
+++ genjson.js
@@ -23,3 +23,54 @@ fs.readFile('bigjson/authtable.json', 'utf8', function(err, data) {
     console.log("totalauth.json has been created! :)")
   })
 } )
+
+
+function drawChart() {
+
+  var data = new google.visualization.DataTable();
+  // datetime format 2016-03-15T15:02:30.000Z
+  $.getJSON('json/authtable.json', function(obj) {
+    var date = new Date(GetURLParameter('date')* 1000);
+    var isoDate = date.toISOString().substring(0, 10);
+    var timeArray = [];
+    $(".page-header").text("Stats for " + isoDate);
+    //I'm fucked
+    obj.forEach(function(arrayItem) {
+      if(timeArray.length === 0) {
+        timeArray.push({"time": "melons", "count": 0});
+      }else {
+        var shortDate = arrayItem.timestamp.substring(0, 10);
+        var roundDateTime = arrayItem.timestamp.substring(0, 14) + "00:00.000Z";
+        if ((isoDate === shortDate) && (roundDateTime !== timeArray[timeArray.length - 1].time) ) {
+          timeArray.push({"time": roundDateTime, "count": 1})
+        } else if (isoDate === shortDate){
+          timeArray[timeArray.length - 1].count += 1;
+        }
+      }
+
+    })
+
+    timeArray.shift();
+
+    data.addColumn('datetime', 'Time');
+    data.addColumn('number', 'Authentication Requests');
+
+    timeArray.forEach( function(arrayItem) {
+      document.getElementById('chart_div').innerHTML = arrayItem.time;
+      data.addRow([new Date(arrayItem.time), arrayItem.count]);
+    });
+    var options = {
+      title: 'Authentication requests for ' + isoDate,
+      height: 400,
+      hAxis: {
+        format: 'hh:mm'
+      }
+    }
+    var chart = new google.visualization.LineChart(document.getElementById('chart_div'));
+
+    chart.draw(data, options);
+
+  })
+
+
+}
--- mysql.js
+++ mysql.js
@@ -15,84 +15,129 @@ connection.connect(function(error){
     //Error: Cannot enqueue Handshake after invoking quit.
     return;
   }
-  console.log('Connection established with ID: ' + connection.threadId);
+  console.log('Connection to DB established with ID: ' + connection.threadId);
 });
 
-// connection.query( 'SELECT sessions.ip, auth.username, auth.password, auth.timestamp '
-//                                 + 'FROM auth '
-//                                 + 'INNER JOIN sessions '
-//                                 + 'ON auth.session = sessions.id '
-//                                 + 'ORDER BY auth.timestamp ASC;',
-//                                 function(error, result) {
-//     if(error){
-//       console.error('Error on queryAuth: ' + error);
-//       return;
-//     }
-//     console.log(result);
-//     fs.writeFile(__dirname + "/bigjson/authtable.json", JSON.stringify(result), function(err) {
-//       if(err) {
-//         return console.log(err);
-//       }
-//       console.log("authtable.json has been created! :)")
-//     })
-//   });
-
-connection.query( 'SELECT DISTINCT ip '
-                  + 'FROM sessions LIMIT 5',
-                   function(error, result) {
-  if(error){
-    console.error('Error on getIP: ' + error);
-    return
-  }
-  console.log(JSON.stringify(result));
-
-  var ipnums= [];
-
-  result.forEach( function(arrayItem) {
-    var currentIpNum = 0;
-
-    var individualNums = arrayItem.ip.split('.');
-
-    currentIpNum += individualNums[0] * 16777216;
-    currentIpNum += individualNums[1] * 65536;
-    currentIpNum += individualNums[2] * 256;
-    currentIpNum += parseInt(individualNums[3]);
-    arrayItem.ipnum = currentIpNum;
-  })
-
-  console.log(JSON.stringify(result));
-  var sql = []
-  result.forEach(function(arrayItem) {
-    sql.push({"ip": arrayItem.ip, "query": 'SELECT country_code, country_name, region_name, city_name, latitude, longitude ' +
-              'FROM ip2location_db5 ' +
-              'WHERE ip_from <= ' + arrayItem.ipnum + ' AND ' + arrayItem.ipnum + ' <= ip_to;'});
-  });
-  console.log(sql);
-
-  sql.forEach(function(arrayItem) {
-    connection.query(arrayItem.query, function(error, result) {
-      if(error) {
-        console.error('Error on selecting IP: ' + error);
-        return
+function updateAuthJson() {
+  connection.query( 'SELECT sessions.ip, auth.username, auth.password, auth.timestamp '
+                                  + 'FROM auth '
+                                  + 'INNER JOIN sessions '
+                                  + 'ON auth.session = sessions.id '
+                                  + 'ORDER BY auth.timestamp ASC;',
+                                  function(error, result) {
+    if(error){
+      console.error('Error on updateAuthJson: ' + error);
+      return;
+    }
+    //console.log(result);
+    fs.writeFile(__dirname + "/bigjson/authtable.json", JSON.stringify(result), function(err) {
+      if(err) {
+        return console.log(err);
       }
-      result[0].ip = arrayItem.ip;
-      console.log(result);
-      var ipdata = result;
-      connection.query("INSERT INTO ip (ip, country_code, country_name, region_name, city_name, latitude, longitude)" +
-                        "VALUES ('" + ipdata[0].ip + "','" + ipdata[0].country_code + "','" + ipdata[0].country_name + "','" + mysql_real_escape_string(ipdata[0].region_name) + "','" + ipdata[0].city_name + "','" + ipdata[0].latitude + "','" + ipdata[0].longitude + "');",
-                      function(error, result) {
-                        if(error) {
-                          console.error('Error on Inserting IP Details: ' + error);
-                          return
-                        }
-                        console.log(result);
-                      })
+      console.log("authtable.json has been created/updated! :)")
     })
   });
+};
 
-  console.log(sql);
+function updateJson(filename, sqlQuery) {
+  connection.query(sqlQuery, function(error, result) {
+    if(error){
+    console.error('Error on updateJson(' + filename + '): ' + error);
+    return;
+    }
+    fs.writeFile(__dirname + "/public/json/" + filename + ".json", JSON.stringify(result), function(err) {
+    if(err) {
+    return console.log(err);
+    }
+    console.log(filename + ".json has been created/updated! :)")
+    })
+  })
+}
 
-});
+function updateIPTable() {
+  console.log("******** Updating IP Table with new IP Information ********\n");
+  console.log("--- Checking for new IPs... ---\n")
+  connection.query( 'SELECT DISTINCT sessions.ip '
+                  + 'FROM sessions '
+                  + 'LEFT OUTER JOIN ip '
+                  + 'ON sessions.ip = ip.ip '
+                  + 'WHERE ip.ip IS NULL;',
+                   function(error, result) {
+    if(error){
+      console.error('Error on lookupIPs: ' + error);
+      return;
+    } else if(result.length === 0) {
+      console.log('\n--- IP table is already up to date :) Nothing to do here... ---');
+      return;
+    }
+    //console.log(JSON.stringify(result));
+
+    var ipnums= [];
+    console.log("----------\nNew IPs Found: \n");
+    result.forEach( function(arrayItem) {
+      var currentIpNum = 0;
+
+      var individualNums = arrayItem.ip.split('.');
+
+      currentIpNum += individualNums[0] * 16777216;
+      currentIpNum += individualNums[1] * 65536;
+      currentIpNum += individualNums[2] * 256;
+      currentIpNum += parseInt(individualNums[3]);
+      arrayItem.ipnum = currentIpNum;
+      console.log(arrayItem.ip + " | Setting IPNum to: " + currentIpNum);
+    })
+
+    var sql = []
+    result.forEach(function(arrayItem) {
+      sql.push({"ip": arrayItem.ip, "query": 'SELECT country_code, country_name, region_name, city_name, latitude, longitude ' +
+                'FROM ip2location_db5 ' +
+                'WHERE ip_from <= ' + arrayItem.ipnum + ' AND ' + arrayItem.ipnum + ' <= ip_to;'});
+    });
+    console.log("----------\nRunning select queries... \n");
+
+    sql.forEach(function(arrayItem) {
+      connection.query(arrayItem.query, function(error, result) {
+        if(error) {
+          console.error('Error on selecting IP: ' + error);
+          return
+        }
+        console.log("Lookup of " + arrayItem.ip + " Successful | Country: " + result[0].country_name);
+        result[0].ip = arrayItem.ip;
+        //console.log(result);
+        var ipdata = result;
+        connection.query("INSERT INTO ip (ip, country_code, country_name, region_name, city_name, latitude, longitude)" +
+                          "VALUES ('" + ipdata[0].ip + "','" + ipdata[0].country_code + "','" + mysql_real_escape_string(ipdata[0].country_name) + "','" + mysql_real_escape_string(ipdata[0].region_name) + "','" + mysql_real_escape_string(ipdata[0].city_name) + "','" + ipdata[0].latitude + "','" + ipdata[0].longitude + "');",
+                        function(error, result) {
+                          if(error) {
+                            console.error('Error on Inserting IP Details: ' + error);
+                            return
+                          }
+                          console.log("*Inserted*: " + ipdata[0].ip + " | Server Status: " + result.serverStatus + " | Affected Rows: " + result.affectedRows);
+                          //console.log(result);
+                        });
+      });
+    });
+  });
+};
+
+// updateIPTable();
+// //updateAuthJson(); //This one is a hassle but keeping here for the sake of a few functions
+// updateJson("totalauthbyday", 'SELECT DATE(timestamp) as date, COUNT(timestamp) as count '
+//                       + 'FROM auth '
+//                       + 'GROUP BY DATE(timestamp);');
+//
+// updateJson("locations", 'SELECT sessions.ip, COUNT(sessions.ip) as count, ip.latitude, ip.longitude '
+//                       + 'FROM auth '
+//                       + 'INNER JOIN sessions '
+//                       + 'ON auth.session = sessions.id '
+//                       + 'JOIN ip '
+//                       + 'ON sessions.ip = ip.ip '
+//                       + 'GROUP BY sessions.ip');
+updateJson("totalauthbyhour", "SELECT concat(DATE(timestamp),' ', HOUR(timestamp), ':00:00') as date, count(timestamp) as count "
+        + "FROM auth "
+        + "GROUP BY date(timestamp), HOUR(timestamp)")
+
+//REENABLE SAFE UPDATES!
 
 function mysql_real_escape_string (str) {
     return str.replace(/[\0\x08\x09\x1a\n\r"'\\\%]/g, function (char) {
@@ -119,5 +164,5 @@ function mysql_real_escape_string (str) {
     });
 }
 
-// module.exports.queryToTable = queryToTable;
+module.exports.updateIPTable = updateIPTable;
 // module.exports.queryAuth = queryAuth;
